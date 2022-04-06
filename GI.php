5afaded57420f28096f6ef85850d972e06125243
DiegoTPinheiro <diegotpinheiro@outlook.com>
2022-04-06 12:13:28 -0300

    Initial commit

---
+++ GI.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:ital,wght@1,300&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+        <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+        <link rel="stylesheet" href="css/fontawesome.min.css">
+        <title>GiBeauty - Estética e Beleza</title>
+    </head>
+    <body>
+        <nav>
+            <div class="logo">
+              <h4>GiBeauty</h4>  
+            </div>
+            <div class="search-box">
+                <input class="search-txt" type="text" name="" placeholder="Faça a sua pesquisa">
+                <a class="search-btn" href="#">
+                <i class="fas fa-search"></i>
+                </a>
+            </div>
+            <ul class="nav-links">
+                <li>
+                    <a href="login.html">Salão</a>
+                </li>
+                <li>
+                    <a href="login.html">Sobre nós</a>
+                </li>
+                <li>
+                    <a href="login.html">Masculino</a>
+                </li>
+                <li>
+                    <a href="login.html">Feminino</a>
+                </li>
+                <li>
+                    <a href="login.html">Entrar</a>
+                </li>
+            </ul>
+            <div class="cart">
+                <div class="ball">
+                <a class="cart-btn" href="#">
+                <i class="fas fa-cart-plus"></i>
+                </a>
+                </div>
+            </div>
+            <div class="burger">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+        </nav>
+        
+        <div class="corpo">
+            <div>
+                
+            </div>
+        </div>
+        
+        <footer>
+        </footer>
+
+        <script src="app.js"></script>
+    </body>
+</html>
